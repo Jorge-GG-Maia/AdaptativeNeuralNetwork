2b9ba818ccbe59293388e7399484679fdc7967d2
Jorge-GG-Maia <54990439+Jorge-GG-Maia@users.noreply.github.com>
2021-09-02 10:44:26 -0300

    v0.0.5
    [PT-BR] Adicionada função simples para pontuação dos acertos da rede neural em casos de predição binária
    
    [ENG] Add simple fitness function for binary predictions

--- RedeAdaptavel.py
+++ RedeAdaptavel.py
@@ -8,7 +8,7 @@ class RedeNeural():
         self.gen = gen
         self.name = str(gen) + '-' + str(number)
         self.pesos = pesos
-        
+        self.score = 0
         
     def genesis(self, amostra, camadas = 3):
     
@@ -50,81 +50,18 @@ class RedeNeural():
             camadaatual = novacamada
         
         self.saida = sum(camadaatual)
-
-    def sigmoid(self, step_value = False):
+        
+        
+        
+    def b_retorno(self, y):
     
-        sigm = 1 / ( 1 + np.exp(-self.saida))
+        if y == 0:
         
-        
-        if step_value == True:
-        
-            if sigm >= 0.5:
-                return 1
-                
-            else:
-                return 0
-                
+            self.score += 1
+            
         else:
-            return sigm
-                        
-    
-    def tanh(self):
         
-        tanh = math.tanh(self.saida)
-        
-        return tanh
-    import numpy as np
-import math
-
-
-class RedeNeural():
-
-    def __init__(self, gen, number, pesos = 0):
-        self.gen = gen
-        self.name = str(gen) + '-' + str(number)
-        self.pesos = pesos
-        
-        
-    def genesis(self, amostra, camadas = 3):
-    
-
-        
-        #Lista principal, contendo o conjunto de listas de cada camada 
-        layer = []
-        
-        for i in range(camadas):
-            #Lista intermediária, contendo as listas de pesos específicos para cada variável 
-            
-            c = []
-            for i in range(len(amostra)):
-                
-                #Criação de uma lista específica de pesos para cada variável presente na amostra 
-                pesos = np.random.randn(len(amostra))
-                
-                c.append(pesos)
-                
-            layer.append(c)
-            
-        self.pesos = layer
-
-
-    def pensar(self, entrada):
-        
-        camadaatual = entrada
-        
-        for i in range(len(self.pesos)):
-            
-            novacamada = []
-            for j in range(len(self.pesos[i])):
-            
-            
-                new_value = sum([x * y for x,y in zip(camadaatual, self.pesos[i][j])])
-                novacamada.append(new_value)
-                
-                
-            camadaatual = novacamada
-        
-        self.saida = sum(camadaatual)
+            self.score -= 1
 
     def sigmoid(self, step_value = False):
     
